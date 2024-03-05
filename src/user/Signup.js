22b2f941510a83c4614587431a14571b3096e661
michaloumen <michelle.mendonca@minu.co>
2024-03-05 08:51:11 -0300

    environment variables

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,11 @@
 import Layout from "../core/Layout";
+import { API } from "../config";
 
 const Signup = () => (
   <Layout
     title='Signup'
     description='Signup to Node React E-commerce App'>
-    ...
+    {API}
   </Layout>
 );
 
