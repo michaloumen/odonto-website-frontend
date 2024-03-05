834c767accf986f57e049ad2cca6dde92b45cf8f
michaloumen <michelle.mendonca@minu.co>
2024-03-05 07:45:33 -0300

    routing pages

--- src/App.js
+++
@@ -1,3 +0,0 @@
-const App = () => (<div>Oi</div>)
-
-export default App;
---
+++ src/user/Routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './Signup';
+import Signin from './Signin';
+import Home from '../core/Home';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
+
+export default App;
