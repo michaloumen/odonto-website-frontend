a2f800e89a2034a845d7f32d86b5faafc82b39cd
michaloumen <michelle.mendonca@minu.co>
2024-03-12 09:52:06 -0300

    show and hide signin signout links conditionally

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { signout } from '../auth';
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (path) => {
   if (window.location.pathname === path) {
@@ -26,37 +26,43 @@ const Menu = () => {
           </Link>
         </li>
 
-        <li className='nav-item' style={isActive('/')}>
-          <Link
-            className='nav-link'
-            style={isActive('/signin')}
-            to='/signin'
-          >
-            Signin
-          </Link>
-        </li>
+        {!isAuthenticated() && (
+          <>
+            <li className='nav-item' style={isActive('/')}>
+              <Link
+                className='nav-link'
+                style={isActive('/signin')}
+                to='/signin'
+              >
+                Signin
+              </Link>
+            </li>
 
-        <li className='nav-item'>
-          <Link
-            className='nav-link'
-            style={isActive('/signup')}
-            to='/signup'
-          >
-            Signup
-          </Link>
-        </li>
+            <li className='nav-item'>
+              <Link
+                className='nav-link'
+                style={isActive('/signup')}
+                to='/signup'
+              >
+                Signup
+              </Link>
+            </li>
+          </>
+        )}
 
-        <li className='nav-item'>
-          <span
-            className='nav-link'
-            style={{ cursor: 'pointer', color: '#ffffff' }}
-            onClick={() => signout(() => {
-              navigate('/');
-            })}
-          >
-            Signout
-          </span>
-        </li>
+        {isAuthenticated() && (
+          <li className='nav-item'>
+            <span
+              className='nav-link'
+              style={{ cursor: 'pointer', color: '#ffffff' }}
+              onClick={() => signout(() => {
+                navigate('/');
+              })}
+            >
+              Signout
+            </span>
+          </li>
+        )}
       </ul>
     </div>
   );
